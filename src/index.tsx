3c60c5dce33a6bccb88de7a35251c3afdb8cfd27
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-02-10 15:32:31 -0800

    --add App component inside index.tsx,--add  file structure for Home and Game component

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+// import Home from './screens/home/home';
+// import Game from './screens/game/game';
+import { Home, Game } from './screens'; // Home and Game are exported from ./screens/index.tsx
+
+export default function App() {
+ 
+  return (
+    <View style={styles.container}>
+      <Home />
+      <Game />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
