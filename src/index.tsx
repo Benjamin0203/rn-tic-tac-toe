07d8c948a1d15fde536bea3683d99d6a76322f21
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-02-11 00:47:27 -0800

    --add customized google font

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 // import Home from './screens/home/home';
 // import Game from './screens/game/game';
-import { Home, Game } from './screens'; // Home and Game are exported from ./screens/index.tsx
+import { Home, Game } from "./screens/index"; // Home and Game are exported from ./screens/index.tsx
 
 export default function App() {
- 
+
   return (
     <View style={styles.container}>
       <Home />
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,11 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import styles from './home.styles';
-
+import { Ubuntu_400Regular, Ubuntu_700Bold } from '@expo-google-fonts/ubuntu';
+import { useFonts } from 'expo-font';
 export default function Home() {
+
+  const [fontsLoaded] = useFonts({
+    Ubuntu_400Regular,
+    Ubuntu_700Bold,
+  });
+  if(!fontsLoaded) return (<View><Text>Loading...</Text></View>)
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text
+      style={{
+        fontFamily: 'Ubuntu_700Bold',
+        fontSize: 30,
+      }}
+      >
+        Home
+        </Text>
+        <Text style={{fontSize: 30}}>Home</Text>
     </View>
   )
 }
